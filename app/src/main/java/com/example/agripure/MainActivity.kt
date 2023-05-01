51508d68de8559fddbbf3e2c105a12534567db81
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-05-01 15:23:25 -0500

    create home activity

--- app/src/main/java/com/example/agripure/MainActivity.kt
+++ app/src/main/java/com/example/agripure/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.agripure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CompoundButton
 import android.widget.Switch
 
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val switchOnOff:Switch=findViewById(R.id.switchOnOff)
+
+        val btnLogin: Button = findViewById(R.id.btnLogIn)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
 
 
     }
